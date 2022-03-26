a6516eb8e168b798b52bb105860574c2dcf77821
Terence NGO <59768868+ngoterence@users.noreply.github.com>
2022-03-26 17:17:02 +0100

    Update classifier.py

--- submissions/starting_kit/classifier.py
+++ submissions/starting_kit/classifier.py
@@ -1,16 +1,29 @@
 from sklearn.ensemble import RandomForestClassifier
-
 from sklearn.base import BaseEstimator
+from sklearn.decomposition import PCA
 
 
 class Classifier(BaseEstimator):
     def __init__(self):
         self.classifier = RandomForestClassifier(
-            n_estimators=100, max_depth=50, max_features=3)
+            n_estimators=100, max_depth=20, random_state=0)
+        
 
     def fit(self, X, y):
-        self.classifier.fit(X, y)
+        """
+        ['Medium', 'Classification', 'Culture',
+       'Object Begin Date', 'height', 'diam', 'width', 'depth']
+        """
+        # selected features: 'Medium', 'Classification', 'Culture'
+        X = X[:,:3]
+        pca = PCA(n_components=2, random_state=1)
+        X_train_pca=pca.fit_transform(X)
+        # store the preprocessor
+        self.preprocessor = pca
+        self.classifier.fit(X_train_pca, y)
 
     def predict_proba(self, X):
-        return self.classifier.predict_proba(X)
+        X = X[:,:3]
+        X_test_pca = self.preprocessor.transform(X)
+        return self.classifier.predict_proba(X_test_pca)
 
